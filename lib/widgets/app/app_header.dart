6c60d9c46975f03a0297415adef0193268a2c2c7
zikwall <andrey.kapitonov.96@gmail.com>
2020-06-28 22:08:33 +0300

    Settings screen

--- lib/widgets/app/app_header.dart
+++ lib/widgets/app/app_header.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:never_have_i_ever/transitions/scale_route.dart';
+import 'package:never_have_i_ever/screens/settings/settings.dart';
 
 class AppHeader extends StatelessWidget {
   final String title;
   final Color color;
+  final Color packColor;
 
   AppHeader({
     @required this.title,
-    @required this.color
+    @required this.color,
+    @required this.packColor
   });
 
   @override
@@ -15,15 +19,21 @@ class AppHeader extends StatelessWidget {
         alignment: Alignment.topCenter,
         child: Padding(
           padding: EdgeInsets.only(
-            left: 20,
+            left: 0,
             top: 10,
-            right: 30,
+            right: 0,
             bottom: 10,
           ),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Icon(Icons.chevron_left, color: Colors.white, size: 45.0),
+              FlatButton(
+                  onPressed: () {
+                   // todo
+                  },
+                  child: Icon(Icons.chevron_left, color: Colors.white, size: 45.0)
+              ),
+              const Spacer(),
               Text(
                 title,
                 style: TextStyle(
@@ -33,7 +43,16 @@ class AppHeader extends StatelessWidget {
                     fontWeight: FontWeight.w700
                 ),
               ),
-              Icon(Icons.settings, color: Colors.white, size: 25.0),
+              const Spacer(),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        ScaleRouteTransition(widget: Settings(color: packColor))
+                    );
+                  },
+                  child: Icon(Icons.settings, color: Colors.white, size: 25.0)
+              ),
             ],
           ),
         )
---
+++ lib/widgets/app/settings_header.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SettingsHeader extends StatelessWidget {
+  final Color color;
+
+  SettingsHeader({
+    @required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      child:
+      Padding(
+        padding: EdgeInsets.only(
+          left: 0,
+          top: 10,
+          right: 0,
+          bottom: 30,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.chevron_left, color: Colors.black, size: 45.0)
+            ),
+            const Spacer(),
+            Text(
+              'настройки',
+              style: TextStyle(
+                fontSize: 35.0,
+                color: color,
+                fontFamily: "Soft",
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const Spacer(flex: 5)
+          ],
+        ),
+      ),
+    );
+  }
+}
